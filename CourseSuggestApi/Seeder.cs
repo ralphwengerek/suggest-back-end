fbb49753ed2d00fd69acdedbaebd1d93443834f2
ralphwengerek <ralph.wengerek@gmail.com>
2018-11-29 16:22:27 +0000

    Updated seed data

--- CourseSuggestApi/Seeder.cs
+++ CourseSuggestApi/Seeder.cs
@@ -39,10 +39,10 @@ namespace CourseSuggestApi
                 {
                     new CourseSuggestion {
                         CourseName = "The Web Developer Bootcamp",
-                        CourseDescription = $@"The only course you need to learn web development - HTML, CSS, JS, Node, and More!\n
-                        Make REAL web applications using cutting-edge technologies\n
-                        Continue to learn and grow as a developer, long after the course ends\n
-                        Create a blog application from scratch using Express, MongoDB, and Semantic UI\n
+                        CourseDescription = $@"The only course you need to learn web development - HTML, CSS, JS, Node, and More!
+                        Make REAL web applications using cutting-edge technologies
+                        Continue to learn and grow as a developer, long after the course ends
+                        Create a blog application from scratch using Express, MongoDB, and Semantic UI
                         Create a complicated yelp-like application from scratch",
                         AbilityLevel = abilityLevels[0],
                         AuthorLevel = "Level 1",
@@ -51,12 +51,12 @@ namespace CourseSuggestApi
                     },
                     new CourseSuggestion {
                         CourseName = "Machine Learning A-Z™: Hands-On Python & R In Data Science",
-                        CourseDescription = $@"Learn to create Machine Learning Algorithms in Python and R from two Data Science experts. Code templates included.\n
-                        Master Machine Learning on Python & R\n
-                        Have a great intuition of many Machine Learning models\n
-                        Make accurate predictions\n
-                        Make powerful analysis\n
-                        Make robust Machine Learning models\n",
+                        CourseDescription = $@"Learn to create Machine Learning Algorithms in Python and R from two Data Science experts. Code templates included.
+                        Master Machine Learning on Python & R
+                        Have a great intuition of many Machine Learning models
+                        Make accurate predictions
+                        Make powerful analysis
+                        Make robust Machine Learning models",
                         AbilityLevel = abilityLevels[0],
                         AuthorLevel = "Level 4",
                         AuthorName = "Olga Ivanova",
@@ -64,7 +64,7 @@ namespace CourseSuggestApi
                     },
                     new CourseSuggestion {
                         CourseName = "The Complete JavaScript Course 2018: Build Real Projects!",
-                        CourseDescription = $@"Master JavaScript with the most complete course on the market! Projects, challenges, quizzes, ES6+, OOP, AJAX, Webpack\n
+                        CourseDescription = $@"Master JavaScript with the most complete course on the market! Projects, challenges, quizzes, ES6+, OOP, AJAX, Webpack
                         Go from a total beginner to an advanced JavaScript developer
                         Code 3 beautiful real-world apps with both ES5 and ES6+ (no boring toy apps)
                         JavaScript and programming fundamentals: variables, boolean logic, if/else, loops, functions, arrays, etc.
@@ -76,12 +76,12 @@ namespace CourseSuggestApi
                     },
                     new CourseSuggestion {
                         CourseName = "The Complete Node.js Developer Course!",
-                        CourseDescription = $@"Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!\n
-                        Build, test, and launch Node apps\n
-                        Create Express web servers and APIs\n
-                        Store data with Mongoose and MongoDB\n
-                        Use cutting-edge ES6/ES7 JavaScript\n
-                        Deploy your Node apps to production\n
+                        CourseDescription = $@"Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!
+                        Build, test, and launch Node apps
+                        Create Express web servers and APIs
+                        Store data with Mongoose and MongoDB
+                        Use cutting-edge ES6/ES7 JavaScript
+                        Deploy your Node apps to production
                         ",
                         AbilityLevel = abilityLevels[0],
                         AuthorLevel = "Level 5",
