6a45f9bb1491c193e452c09e8f6aaeb8de6fcb3a
ralphwengerek <ralphwengerek@rwengerek.local>
2018-11-29 11:02:39 +0000

    Created date migrations

---
+++ CourseSuggestApi.Db/Migrations/20181129110024_AddedCreatedDate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CourseSuggestApi.Db.Migrations
+{
+    public partial class AddedCreatedDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Votes",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "CourseSuggestions",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Votes");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "CourseSuggestions");
+        }
+    }
+}
